b1ed7c70ea517fcc0466d0c230b3baa55f657d06
nik <niklas.harnish@hotmail.com>
2024-04-07 15:50:54 +0200

    added most of the UI stuff

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,85 @@
-use gtk::{glib::clone, prelude::*, glib};
+use gtk::{
+    gdk::Display,
+    glib::{self, clone},
+    prelude::*,
+    CssProvider,
+};
 
 const APP_ID: &str = "gtk.todo_list";
 const DEFAULT_WINDOW_DIMENSIONS: (i32, i32) = (600, 800);
 fn main() {
     let app = gtk::Application::builder().application_id(APP_ID).build();
-
+    app.connect_startup(|_| load_css());
     app.connect_activate(app_on_activate);
     app.run();
 }
 
 fn app_on_activate(app: &gtk::Application) {
-    let gtk_box = gtk::Box::builder()
+    let content_body = gtk::Box::builder()
         .orientation(gtk::Orientation::Vertical)
         .build();
 
-    let button_box = gtk::Box::builder()
+    let task_div = gtk::Box::builder()
         .orientation(gtk::Orientation::Vertical)
         .build();
 
-    let emoji_chooser = gtk::EmojiChooser::builder()
-        .margin_start(10)
-        .margin_end(10)
-        .margin_top(10)
-        .margin_bottom(10)
+    let title = gtk::Label::builder()
+        .label("Todo List")
+        .margin_bottom(20)
         .build();
+    title.add_css_class("title_text");
 
-    emoji_chooser
-        .connect_emoji_picked(|_emoji_chooser, picked| println!("User picked the emoji: {picked}"));
+    let add_item_textbox = gtk::Text::builder().margin_end(5).build();
+    add_item_textbox.add_css_class("add_item_textbox");
+    let add_item_button = gtk::Button::builder().label("Add Item").build();
 
-    let text = gtk::Label::builder().label("Todo List").build();
-    let button = gtk::Button::builder().label("Emojis").build();
-
-    button.connect_clicked(clone!(@strong emoji_chooser =>  move |_button| {
-        emoji_chooser.set_visible(!emoji_chooser.get_visible())
+    add_item_button.connect_clicked(clone!(@weak task_div, @weak add_item_textbox =>  move |_button| {
+        let new_task_text = add_item_textbox.text();
+        let new_task = gtk::Label::builder().label(new_task_text).margin_top(10).build();
+        task_div.append(&new_task);
     }));
 
-    button_box.append(&button);
-    button_box.append(&emoji_chooser);
+    let new_task_div = gtk::Grid::builder()
+        .orientation(gtk::Orientation::Horizontal)
+        .margin_start(10)
+        .margin_end(10)
+        .margin_bottom(20)
+        .margin_top(20)
+        .build();
 
-    gtk_box.append(&text);
-    gtk_box.append(&button_box);
+    new_task_div.add_css_class("new_task");
+    new_task_div.set_halign(gtk::Align::Center);
+
+    const ADD_ITEM_TEXTBOX_WIDTH: i32 = 200;
+    new_task_div.attach(&add_item_textbox, 0, 0, ADD_ITEM_TEXTBOX_WIDTH, 1);
+    new_task_div.attach(&add_item_button, ADD_ITEM_TEXTBOX_WIDTH + 1, 0, 40, 1);
+
+    let new_task_divider = gtk::Separator::builder()
+        .orientation(gtk::Orientation::Horizontal)
+        .build();
+
+    content_body.append(&title);
+    content_body.append(&new_task_div);
+    content_body.append(&new_task_divider);
+    content_body.append(&task_div);
 
     let window = gtk::Window::builder()
         .application(app)
         .title("Todo List")
-        .child(&gtk_box)
+        .child(&content_body)
         .default_width(DEFAULT_WINDOW_DIMENSIONS.0)
         .default_height(DEFAULT_WINDOW_DIMENSIONS.1)
         .build();
 
     window.present();
+}
+
+fn load_css() {
+    let provider = CssProvider::new();
+    provider.load_from_string(include_str!("css/styles.css"));
+    gtk::style_context_add_provider_for_display(
+        &Display::default().expect("Could not connect to a display."),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
 }
